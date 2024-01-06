5bca85d5ddef3e117f3e8ca226dac41875557385
Alexey Matyunin <alexey.matyunin@vk.team>
2024-01-06 14:13:21 +0300

    fix greeting

--- src/app.py
+++ src/app.py
@@ -46,7 +46,7 @@ async def get_weather(city) -> str:
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Hello, {hbold(message.from_user.full_name)}!")
+    await message.answer(f"Hello 👋, {hbold(message.from_user.full_name)}!")
 
 
 @dp.message()
